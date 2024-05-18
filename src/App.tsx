f78dfec4cf2f34d54ddeeec7749f53d068955e5e
tkanastasi <tkacheva.anastasi@gmail.com>
2024-05-18 09:33:01 +0400

    todo layout is pretty fine

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <div className="container mt-4 vh-100">
+      <div className="container mt-4">
         {/* Title */}
         <div className="row">
           <div className="col">
@@ -20,15 +20,12 @@ function App() {
           <div className="col-md-6">
             <button className="btn btn-primary">Create New Task</button>
           </div>
-          <div className="col-md-6 text-right">
-            <span className="h5">Frustration Level: 5 😟</span>
-          </div>
         </div>
 
         {/* Task Table */}
-        <div className="row mt-4 v-100">
-          <div className="col-12 table-container v-100">
-            <table className="table table-striped v-100">
+        <div className="row mt-4">
+          <div className="table-container">
+            <table className="table table-striped">
               <thead>
                 <tr>
                   <th>Task Order</th>
@@ -37,7 +34,7 @@ function App() {
                   <th>Story Points</th>
                 </tr>
               </thead>
-              <tbody className="vh-100">
+              <tbody>
                 <tr>
                   <td>1</td>
                   <td>Complete the project documentation</td>
@@ -50,76 +47,15 @@ function App() {
                   <td>Moderate</td>
                   <td>8</td>
                 </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Implement authentication</td>
-                  <td>Moderate</td>
-                  <td>8</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Implement authentication</td>
-                  <td>Moderate</td>
-                  <td>8</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Implement authentication</td>
-                  <td>Moderate</td>
-                  <td>8</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Implement authentication</td>
-                  <td>Moderate</td>
-                  <td>8</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Implement authentication</td>
-                  <td>Moderate</td>
-                  <td>8</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Implement authentication</td>
-                  <td>Moderate</td>
-                  <td>8</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Implement authentication</td>
-                  <td>Moderate</td>
-                  <td>8</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Implement authentication</td>
-                  <td>Moderate</td>
-                  <td>8</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Implement authentication</td>
-                  <td>Moderate</td>
-                  <td>8</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Implement authentication</td>
-                  <td>Moderate</td>
-                  <td>8</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Implement authentication</td>
-                  <td>Moderate</td>
-                  <td>8</td>
-                </tr>
-
                 {/* Additional tasks here */}
               </tbody>
             </table>
+          </div>
+        </div>
+
+        <div className="row mt-4">
+          <div className="col-md-6 text-right">
+            <span className="h5">Frustration Level: 5 😟</span>
           </div>
         </div>
       </div>
