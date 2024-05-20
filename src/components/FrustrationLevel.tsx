8466daf9a95b50c96535118b0204080481d70072
tkanastasi <tkacheva.anastasi@gmail.com>
2024-05-20 11:07:28 +0400

    exponential modulation of font size

--- src/components/FrustrationLevel.tsx
+++ src/components/FrustrationLevel.tsx
@@ -41,13 +41,11 @@ export const FrustrationLevel: React.FC<FrustrationLevelProps> = ({ temp }) => {
       fetchCounter.current++;
 
       try {
-          const response = await fetch(`http://www.filltext.com/?rows=${frustrationEmojis.length}&x={decimalRange|4,10}`);
+          const response = await fetch(`http://www.filltext.com/?rows=${frustrationEmojis.length - 1}&x={decimalRange|4,10}`);
           const json = await response.json();
 
           const rNumbers: number[] = json.map((item: { x: string }) => parseFloat(item.x));
           const array = partialSums(rNumbers.sort((a, b) => a - b));
-          console.log(`rNumbers: ${rNumbers}`)
-          console.log(`array: ${array}`)
           setFrustrationBounds(array);
       } catch (error) {
           console.error('Error fetching data:', error);
@@ -59,8 +57,23 @@ export const FrustrationLevel: React.FC<FrustrationLevelProps> = ({ temp }) => {
 
   const frustrationLevel = factorial(BigInt(temp));
   const emojiIdx = frustrationBounds?.reduce((idx, v) => (temp > v ? idx + 1 : idx), 0);
-
+  const baseFontSize = 2;
+  
   return (
-    <span className="h5">Frustration Level: {frustrationLevel.toString()} {emojiIdx !== undefined ? frustrationEmojis[emojiIdx] : ''}</span>
+    <span>
+      <span key="emoji" style={{ fontSize: `${0.9 * baseFontSize}em` }}>
+        Frustration Level:
+      </span>
+      {emojiIdx !== undefined && (
+        <span key="emoji" style={{ fontSize: `${baseFontSize}em` }}>
+          {frustrationEmojis[emojiIdx]}
+        </span>
+      )}
+      {frustrationLevel.toString().split('').slice(0, 60).map((digit, index) => (
+          <span key={index} style={{ fontSize: `${baseFontSize * Math.exp(-Math.floor(index / 1) * 0.08)}em` }}>
+              {digit}
+          </span>
+      ))}
+    </span>
   );
 }
