17b604e9a706b6dbc835de300c77e8efb23ccd1c
tkanastasi <tkacheva.anastasi@gmail.com>
2024-05-20 10:24:22 +0400

    implementation of frustration boundaries

--- src/components/FrustrationLevel.tsx
+++ src/components/FrustrationLevel.tsx
@@ -1,6 +1,4 @@
-type FrustrationLevelProps = {
-  temp: number
-}
+import React, { useState, useEffect } from 'react'
 
 function factorial(n: bigint){
   let acc = BigInt(1);
@@ -11,9 +9,49 @@ function factorial(n: bigint){
   return acc;
 }
 
+function partialSums(numbers: number[]): number[] {
+  let arr = [];
+
+  for (let acc = 0, i = 0; i < numbers.length; i++){
+    acc += numbers[i];
+    arr.push(acc);
+  }
+
+  return arr;
+}
+
+const frustrationEmojis: string[] = ["😊", "😐", "😣", "😫", "😱"];
+
+type FrustrationLevelProps = {
+  temp: number
+}
+
 export const FrustrationLevel: React.FC<FrustrationLevelProps> = ({ temp }) => {
-  const level = factorial(BigInt(temp));
+  const [frustrationBounds, setFrustrationBounds] = useState<number[]|null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+          const response = await fetch(`http://www.filltext.com/?rows=${frustrationEmojis.length}&x={decimalRange|4,10}`);
+          const json = await response.json();
+
+          const rNumbers: number[] = json.map((item: { x: string }) => parseFloat(item.x));
+          const array = partialSums(rNumbers.sort((a, b) => a - b));
+          console.log(`rNumbers: ${rNumbers}`)
+          console.log(`array: ${array}`)
+          setFrustrationBounds(array);
+      } catch (error) {
+          console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const frustrationLevel = factorial(BigInt(temp));
+  const emojiIdx = frustrationBounds?.reduce((idx, v) => (temp > v ? idx + 1 : idx), 0);
+
   return (
-    <span className="h5">Frustration Level: {level.toString()} 😟</span>
+    <span className="h5">Frustration Level: {frustrationLevel.toString()} {emojiIdx !== undefined ? frustrationEmojis[emojiIdx] : ''}</span>
   );
 }
