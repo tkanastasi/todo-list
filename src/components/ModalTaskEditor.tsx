2a6c1420b5119b1ff9de584e4c7d4c4efd5972b2
tkanastasi <tkacheva.anastasi@gmail.com>
2024-05-22 10:13:20 +0400

    fix: absence of a responsibility of X for closing modal window

--- src/components/ModalTaskEditor.tsx
+++ src/components/ModalTaskEditor.tsx
@@ -99,7 +99,7 @@ export const ModalTaskEditor: React.FC<TaskEditorProps> = ({ taskEditorMode }) =
         <div className="modal-content">
           <div className="modal-header">
             <h5 className="modal-title" id="taskModalLabel">{dialogTitle}</h5>
-            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={cancel}></button>
           </div>
           <div className="modal-body">
             <form>
