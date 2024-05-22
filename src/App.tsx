728148cd9f02e4237522210d5fcd83310076b0f0
tkanastasi <tkacheva.anastasi@gmail.com>
2024-05-22 10:05:36 +0400

    removed unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { TaskList } from './components/TaskList'
-import { ModalTaskEditor } from './components/ModalTaskEditor'
 
 function App() {
   return (
